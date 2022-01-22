06183f67f420fabf639636eab0e9347e33a0e69e
Alkım Deniz Altıntaş <altalk.23@robcol.k12.tr>
2022-01-22 17:02:00 +0300

    mod

--- src/platform/macos/Mod.mm
+++ src/platform/macos/Mod.mm
@@ -7,38 +7,34 @@
 USE_GEODE_NAMESPACE();
 
 Result<> Mod::loadPlatformBinary() {
-	void* dylib = dlopen((this->m_tempDirName / this->m_info.m_binaryName).string().c_str(), RTLD_NOW);
+	auto dylib = dlopen((this->m_tempDirName / this->m_info.m_binaryName).string().c_str(), RTLD_NOW);
     if (dylib) {
-        this->m_loadFunc   = reinterpret_cast<geode_load>(  GetProcAddress(load, "geode_load"));
-        this->m_unloadFunc = reinterpret_cast<geode_unload>(GetProcAddress(load, "geode_unload"));
+        this->m_loadFunc   = reinterpret_cast<geode_load>(  dlsym(dylib, "geode_load"));
+        if (!this->m_loadFunc) 
+        	this->m_loadFunc   = reinterpret_cast<geode_load>(  dlsym(dylib, "_geode_load"));
+       	if (!this->m_loadFunc) 
+       		return Err<>("Unable to find mod entry point");
 
-        if (
-            !this->m_loadFunc &&
-            !(this->m_loadFunc = reinterpret_cast<geode_load>(  GetProcAddress(load, "_geode_load@4")))
-        ) {
-            return Err<>("Unable to find mod entry point");
-        }
-        if (
-            !this->m_unloadFunc &&
-            !(this->m_unloadFunc = reinterpret_cast<geode_unload>(  GetProcAddress(load, "_geode_unload@0")))
-        ) {
-            return Err<>("Unable to find mod unload function");
-        }
+        this->m_unloadFunc = reinterpret_cast<geode_unload>(dlsym(dylib, "geode_unload"));
+        if (!this->m_unloadFunc) 
+        	this->m_unloadFunc   = reinterpret_cast<geode_unload>(  dlsym(dylib, "_geode_unload"));
+       	if (!this->m_unloadFunc) 
+       		return Err<>("Unable to find mod unload function");
 
         if (this->m_platformInfo) {
             delete this->m_platformInfo;
         }
-        this->m_platformInfo = new PlatformInfo { load };
+        this->m_platformInfo = new PlatformInfo { dylib };
 
         return Ok<>();
     }
-    return Err<>("Unable to load the DLL");
+    return Err<>("Unable to load the DYLIB");
 }
 
 Result<> Mod::unloadPlatformBinary() {
-    auto hmod = this->m_platformInfo->m_hmod;
+    auto dylib = this->m_platformInfo->m_dylib;
     delete this->m_platformInfo;
-    if (FreeLibrary(hmod)) {
+    if (dlclose(dylib)) {
         this->m_unloadFunc = nullptr;
         this->m_loadFunc = nullptr;
         return Ok<>();
