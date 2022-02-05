1b18425776aa7d09accae12f310bd723828fdf69
HJfod <60038575+HJfod@users.noreply.github.com>
2022-02-05 19:05:55 +0200

    no cosmic rays pretty please

--- src/hooks/save.cpp
+++ src/hooks/save.cpp
@@ -4,6 +4,7 @@ USE_GEODE_NAMESPACE();
 
 class $modify(AppDelegate) {
     void trySaveGame() {
+        Loader::getInternalMod()->log() << "Saving..." << geode::endl;
         for (auto mod : Loader::get()->getLoadedMods()) {
             auto r = mod->saveData();
             if (!r) Loader::getInternalMod()->logInfo(r.error(), Severity::Error);
