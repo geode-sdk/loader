00e14e8e28e7c22c400f730d4bd8546fee19c2d9
Alkım Deniz Altıntaş <altalk.23@robcol.k12.tr>
2022-02-06 21:34:47 +0300

    addresser

--- src/platform/macos/Mod.mm
+++ src/platform/macos/Mod.mm
@@ -7,42 +7,48 @@
 
 USE_GEODE_NAMESPACE();
 
+template<typename T>
+T findSymbolOrMangled(void* dylib, const char* name, const char* mangled) {
+	auto res = reinterpret_cast<T>(dlsym(dylib, name));
+	if (!res) {
+		res = reinterpret_cast<T>(dlsym(dylib,mangled));
+	}
+	return res;
+}
+
 Result<> Mod::loadPlatformBinary() {
 	auto dylib = dlopen((this->m_tempDirName / this->m_info.m_binaryName).string().c_str(), RTLD_NOW);
-    if (dylib) {
-        this->m_loadFunc   = reinterpret_cast<geode_load>(dlsym(dylib, "geode_load"));
-        if (!this->m_loadFunc) 
-        	this->m_loadFunc   = reinterpret_cast<geode_load>(dlsym(dylib, "_geode_load"));
-       	if (!this->m_loadFunc) 
-       		return Err<>("Unable to find mod entry point");
+	if (dylib) {
+		this->m_implicitLoadFunc = findSymbolOrMangled<geode_load>(dylib, "geode_implicit_load", "_geode_implicit_load");
+		this->m_loadFunc = findSymbolOrMangled<geode_load>(dylib, "geode_load", "_geode_load");
+		this->m_unloadFunc = findSymbolOrMangled<geode_unload>(dylib, "geode_unload", "_geode_unload");
 
-        this->m_unloadFunc = reinterpret_cast<geode_unload>(dlsym(dylib, "geode_unload"));
-        if (!this->m_unloadFunc) 
-        	this->m_unloadFunc   = reinterpret_cast<geode_unload>(dlsym(dylib, "_geode_unload"));
-       	if (!this->m_unloadFunc) 
-       		return Err<>("Unable to find mod unload function");
+		if (!this->m_implicitLoadFunc && !this->m_loadFunc) {
+			return Err<>("Unable to find mod entry point (lacking both implicit & explicit definition)");
+		}
 
-        if (this->m_platformInfo) {
-            delete this->m_platformInfo;
-        }
-        this->m_platformInfo = new PlatformInfo { dylib };
+		if (this->m_platformInfo) {
+			delete this->m_platformInfo;
+		}
+		this->m_platformInfo = new PlatformInfo { dylib };
 
-        return Ok<>();
-    }
-    return Err<>("Unable to load the DYLIB");
+		return Ok<>();
+	}
+	return Err<>("Unable to load the DYLIB");
 }
 
 Result<> Mod::unloadPlatformBinary() {
-    auto dylib = this->m_platformInfo->m_dylib;
-    delete this->m_platformInfo;
-    this->m_platformInfo = nullptr;
-    if (dlclose(dylib) == 0) {
-        this->m_unloadFunc = nullptr;
-        this->m_loadFunc = nullptr;
-        return Ok<>();
-    } else {
-        return Err<>("Unable to free library");
-    }
+	auto dylib = this->m_platformInfo->m_dylib;
+	delete this->m_platformInfo;
+	this->m_platformInfo = nullptr;
+	if (dlclose(dylib) == 0) {
+		this->m_unloadFunc = nullptr;
+		this->m_loadFunc = nullptr;
+		this->m_implicitLoadFunc = nullptr;
+		return Ok<>();
+	} else {
+		return Err<>("Unable to free library");
+	}
 }
 
 #endif
--- src/utils/addresser.cpp
+++ src/utils/addresser.cpp
@@ -13,100 +13,96 @@
 
 #if INT64_MAX == INTPTR_MAX
 	#define GEODE_ADDRESSER_NEST1(macro, begin)       \
-	macro(GEODE_CONCAT(begin, 0))                     \
-	macro(GEODE_CONCAT(begin, 8))         
+	macro(GEODE_CONCAT(begin, 0)),                    \
+	macro(GEODE_CONCAT(begin, 8))        
 #else
 	#define GEODE_ADDRESSER_NEST1(macro, begin)       \
-	macro(GEODE_CONCAT(begin, 0))                     \
-	macro(GEODE_CONCAT(begin, 4))                     \
-	macro(GEODE_CONCAT(begin, 8))                     \
+	macro(GEODE_CONCAT(begin, 0)),                    \
+	macro(GEODE_CONCAT(begin, 4)),                    \
+	macro(GEODE_CONCAT(begin, 8)),                    \
 	macro(GEODE_CONCAT(begin, c))         
 #endif
 
 #define GEODE_ADDRESSER_NEST2(macro, begin)           \
-GEODE_ADDRESSER_NEST1(macro, GEODE_CONCAT(begin, 0))  \
-GEODE_ADDRESSER_NEST1(macro, GEODE_CONCAT(begin, 1))  \
-GEODE_ADDRESSER_NEST1(macro, GEODE_CONCAT(begin, 2))  \
-GEODE_ADDRESSER_NEST1(macro, GEODE_CONCAT(begin, 3))  \
-GEODE_ADDRESSER_NEST1(macro, GEODE_CONCAT(begin, 4))  \
-GEODE_ADDRESSER_NEST1(macro, GEODE_CONCAT(begin, 5))  \
-GEODE_ADDRESSER_NEST1(macro, GEODE_CONCAT(begin, 6))  \
-GEODE_ADDRESSER_NEST1(macro, GEODE_CONCAT(begin, 7))  \
-GEODE_ADDRESSER_NEST1(macro, GEODE_CONCAT(begin, 8))  \
-GEODE_ADDRESSER_NEST1(macro, GEODE_CONCAT(begin, 9))  \
-GEODE_ADDRESSER_NEST1(macro, GEODE_CONCAT(begin, a))  \
-GEODE_ADDRESSER_NEST1(macro, GEODE_CONCAT(begin, b))  \
-GEODE_ADDRESSER_NEST1(macro, GEODE_CONCAT(begin, c))  \
-GEODE_ADDRESSER_NEST1(macro, GEODE_CONCAT(begin, d))  \
-GEODE_ADDRESSER_NEST1(macro, GEODE_CONCAT(begin, e))  \
+GEODE_ADDRESSER_NEST1(macro, GEODE_CONCAT(begin, 0)), \
+GEODE_ADDRESSER_NEST1(macro, GEODE_CONCAT(begin, 1)), \
+GEODE_ADDRESSER_NEST1(macro, GEODE_CONCAT(begin, 2)), \
+GEODE_ADDRESSER_NEST1(macro, GEODE_CONCAT(begin, 3)), \
+GEODE_ADDRESSER_NEST1(macro, GEODE_CONCAT(begin, 4)), \
+GEODE_ADDRESSER_NEST1(macro, GEODE_CONCAT(begin, 5)), \
+GEODE_ADDRESSER_NEST1(macro, GEODE_CONCAT(begin, 6)), \
+GEODE_ADDRESSER_NEST1(macro, GEODE_CONCAT(begin, 7)), \
+GEODE_ADDRESSER_NEST1(macro, GEODE_CONCAT(begin, 8)), \
+GEODE_ADDRESSER_NEST1(macro, GEODE_CONCAT(begin, 9)), \
+GEODE_ADDRESSER_NEST1(macro, GEODE_CONCAT(begin, a)), \
+GEODE_ADDRESSER_NEST1(macro, GEODE_CONCAT(begin, b)), \
+GEODE_ADDRESSER_NEST1(macro, GEODE_CONCAT(begin, c)), \
+GEODE_ADDRESSER_NEST1(macro, GEODE_CONCAT(begin, d)), \
+GEODE_ADDRESSER_NEST1(macro, GEODE_CONCAT(begin, e)), \
 GEODE_ADDRESSER_NEST1(macro, GEODE_CONCAT(begin, f))  
 
 #define GEODE_ADDRESSER_NEST3(macro, begin)           \
-GEODE_ADDRESSER_NEST2(macro, GEODE_CONCAT(begin, 0))  \
-GEODE_ADDRESSER_NEST2(macro, GEODE_CONCAT(begin, 1))  \
-GEODE_ADDRESSER_NEST2(macro, GEODE_CONCAT(begin, 2))  \
-GEODE_ADDRESSER_NEST2(macro, GEODE_CONCAT(begin, 3))  \
-GEODE_ADDRESSER_NEST2(macro, GEODE_CONCAT(begin, 4))  \
-GEODE_ADDRESSER_NEST2(macro, GEODE_CONCAT(begin, 5))  \
-GEODE_ADDRESSER_NEST2(macro, GEODE_CONCAT(begin, 6))  \
-GEODE_ADDRESSER_NEST2(macro, GEODE_CONCAT(begin, 7))  \
-GEODE_ADDRESSER_NEST2(macro, GEODE_CONCAT(begin, 8))  \
-GEODE_ADDRESSER_NEST2(macro, GEODE_CONCAT(begin, 9))  \
-GEODE_ADDRESSER_NEST2(macro, GEODE_CONCAT(begin, a))  \
-GEODE_ADDRESSER_NEST2(macro, GEODE_CONCAT(begin, b))  \
-GEODE_ADDRESSER_NEST2(macro, GEODE_CONCAT(begin, c))  \
-GEODE_ADDRESSER_NEST2(macro, GEODE_CONCAT(begin, d))  \
-GEODE_ADDRESSER_NEST2(macro, GEODE_CONCAT(begin, e))  \
+GEODE_ADDRESSER_NEST2(macro, GEODE_CONCAT(begin, 0)), \
+GEODE_ADDRESSER_NEST2(macro, GEODE_CONCAT(begin, 1)), \
+GEODE_ADDRESSER_NEST2(macro, GEODE_CONCAT(begin, 2)), \
+GEODE_ADDRESSER_NEST2(macro, GEODE_CONCAT(begin, 3)), \
+GEODE_ADDRESSER_NEST2(macro, GEODE_CONCAT(begin, 4)), \
+GEODE_ADDRESSER_NEST2(macro, GEODE_CONCAT(begin, 5)), \
+GEODE_ADDRESSER_NEST2(macro, GEODE_CONCAT(begin, 6)), \
+GEODE_ADDRESSER_NEST2(macro, GEODE_CONCAT(begin, 7)), \
+GEODE_ADDRESSER_NEST2(macro, GEODE_CONCAT(begin, 8)), \
+GEODE_ADDRESSER_NEST2(macro, GEODE_CONCAT(begin, 9)), \
+GEODE_ADDRESSER_NEST2(macro, GEODE_CONCAT(begin, a)), \
+GEODE_ADDRESSER_NEST2(macro, GEODE_CONCAT(begin, b)), \
+GEODE_ADDRESSER_NEST2(macro, GEODE_CONCAT(begin, c)), \
+GEODE_ADDRESSER_NEST2(macro, GEODE_CONCAT(begin, d)), \
+GEODE_ADDRESSER_NEST2(macro, GEODE_CONCAT(begin, e)), \
 GEODE_ADDRESSER_NEST2(macro, GEODE_CONCAT(begin, f))  
 
-/**
- * static ptrdiff_t f000() {return 0x000;}
- * static ptrdiff_t f004() {return 0x004;}
- * static ptrdiff_t f008() {return 0x008;}
- * ...
- */
-#define GEODE_ADDRESSER_METHOD_DEFINE(hex)           \
-inline static ptrdiff_t GEODE_CONCAT(f, hex)() {return GEODE_CONCAT(0x, hex);}
 
-/**
- * (intptr_t)Addresser::f000,
- * (intptr_t)Addresser::f004,
- * (intptr_t)Addresser::f008,
- * ...
- */
-#define GEODE_ADDRESSER_TABLE_DEFINE(hex)            \
-(intptr_t)GEODE_CONCAT(AddresserTable::f, hex),
+#define GEODE_ADDRESSER_THUNK0_DEFINE(hex) (intptr_t)&f<GEODE_CONCAT(0x, hex), 0>
+#define GEODE_ADDRESSER_THUNK_DEFINE(hex) (intptr_t)&f<GEODE_CONCAT(0x, hex), I>
+#define GEODE_ADDRESSER_TABLE_DEFINE(hex) (intptr_t)&ThunkTable<GEODE_CONCAT(0x, hex)>::table
 
-#define GEODE_ADDRESSER_METHOD_SET() GEODE_ADDRESSER_NEST3(GEODE_ADDRESSER_METHOD_DEFINE, )
+#define GEODE_ADDRESSER_THUNK0_SET() GEODE_ADDRESSER_NEST3(GEODE_ADDRESSER_THUNK0_DEFINE, )
+#define GEODE_ADDRESSER_THUNK_SET() GEODE_ADDRESSER_NEST2(GEODE_ADDRESSER_THUNK_DEFINE, )
 #define GEODE_ADDRESSER_TABLE_SET() GEODE_ADDRESSER_NEST3(GEODE_ADDRESSER_TABLE_DEFINE, )
 
 using namespace geode::addresser;
 
 namespace {
-	class GEODE_HIDDEN AddresserTable {
+	template<ptrdiff_t index, ptrdiff_t thunk>
+	GEODE_HIDDEN virtual_meta_t f() {
+		return {index, thunk};
+	}
+
+	using thunk0_table_t = intptr_t[0x1000 / sizeof(intptr_t)];
+	using thunk_table_t = intptr_t[0x100 / sizeof(intptr_t)];
+	using table_table_t = intptr_t[0x1000 / sizeof(intptr_t)];
+
+	template <ptrdiff_t I>
+	struct GEODE_HIDDEN ThunkTable {
+		static inline thunk_table_t table = {
+			GEODE_ADDRESSER_THUNK_SET()
+		};
+	};
+
+	template <>
+	struct GEODE_HIDDEN ThunkTable<0> {
+		static inline thunk0_table_t table = {
+			GEODE_ADDRESSER_THUNK0_SET()
+		};
+	};
+
+	class GEODE_HIDDEN TableTable {
 		friend class geode::addresser::Addresser;
 
-		static constexpr ptrdiff_t table_size = 0x1000 / sizeof(intptr_t);
-
-		using table_t = intptr_t[table_size + 0x1]; 
-		using tableptr_t = table_t*; 
-
-		GEODE_ADDRESSER_METHOD_SET()
-		inline static ptrdiff_t f() {return -1;} //because c++ cries when there is a trailing comma 
-
-		inline static tableptr_t tableptr() {
-			static table_t ret = {
-				GEODE_ADDRESSER_TABLE_SET()
-				(intptr_t)AddresserTable::f
-			};
-			return &ret;
-		}
-
-		
+		static inline table_table_t table = {
+			GEODE_ADDRESSER_TABLE_SET()
+		};
 	};
 }
 
-geode::addresser::Addresser* geode::addresser::Addresser::instance() {
-	static auto ret = AddresserTable::tableptr();
-	return reinterpret_cast<geode::addresser::Addresser*>(&ret);
+Addresser* Addresser::instance() {
+	return reinterpret_cast<Addresser*>(&TableTable::table);
 }
