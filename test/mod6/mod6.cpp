f0a15fab15e21e6e03c6581e306a09f9a65ae7d3
altalk23 <altalk23@users.noreply.github.com>
2022-03-01 15:01:04 +0300

    merge + disable mod6

--- test/mod6/mod6.cpp
+++ test/mod6/mod6.cpp
@@ -2,20 +2,20 @@
 
 USE_GEODE_NAMESPACE();
 
-class $modify(CustomListView) {
-	static CustomListView* create(cocos2d::CCArray* items, float height, float width, int page, BoomListType type) {
-		Interface::mod()->log() << "CustomListView::create -> height: " << height << " width: " << width << " type: " << (int)type;
-		return CustomListView::create(items, height, width, page, type);
-	}
-};
+// class $modify(CustomListView) {
+// 	static CustomListView* create(cocos2d::CCArray* items, float height, float width, int page, BoomListType type) {
+// 		Interface::mod()->log() << "CustomListView::create -> height: " << height << " width: " << width << " type: " << (int)type;
+// 		return CustomListView::create(items, height, width, page, type);
+// 	}
+// };
 
 
-class $modify(BoomListView) {
-	bool init(cocos2d::CCArray* items, float height, float width, int page, BoomListType type) {
-		Interface::mod()->log() << "BoomListView::init -> height: " << height << " width: " << width << " type: " << (int)type;
-		return BoomListView::init(items, height, width, page, type);
-	}
-};
+// class $modify(BoomListView) {
+// 	bool init(cocos2d::CCArray* items, float height, float width, int page, BoomListType type) {
+// 		Interface::mod()->log() << "BoomListView::init -> height: " << height << " width: " << width << " type: " << (int)type;
+// 		return BoomListView::init(items, height, width, page, type);
+// 	}
+// };
 
 /* struct reordering not implemented yet
 class $modify(CCLightFlash) {
