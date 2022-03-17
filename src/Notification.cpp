2a6eecc13d1cd4e6af5e2f3acd6053f36b3c911d
camila314 <camden7306@gmail.com>
2022-03-17 12:22:12 -0500

    firecubez's idea on static event stuff

--- src/Notification.cpp
+++ src/Notification.cpp
@@ -15,7 +15,7 @@ NotificationCenter* NotificationCenter::get() {
 	return NotificationCenter::shared;
 }
 
-void NotificationCenter::send(Notification n, Mod* m) {
+/*void NotificationCenter::send(Notification n, Mod* m) {
 	for (auto& obs : m_observers[m][n.selector()]) {
 		obs->m_callback(n);
 	}
@@ -38,19 +38,22 @@ Observer* NotificationCenter::registerObserver(Mod* m, std::string selector, cal
 	m_observers[m][selector].push_back(ob);
 
 	return ob;
-}
+}*/
 
-void NotificationCenter::unregisterObserver(Observer* ob) {
-	auto& v2 = m_observers[ob->m_mod][ob->m_selector];
+void NotificationCenter::unregisterObserver(Observer<std::monostate>* ob) {
+	auto& v2 = m_observers[ob->m_mod][ob->m_info.selector];
 	v2.erase(std::remove(v2.begin(), v2.end(), ob), v2.end());
+
+	if (v2.empty())
+		m_observers[ob->m_mod].erase(ob->m_info.selector);
 	delete ob;
 }
 
-std::vector<Observer*> NotificationCenter::getObservers(std::string selector, Mod* m) {
+std::vector<Observer<std::monostate>*> NotificationCenter::getObservers(std::string selector, Mod* m) {
 	if (m) {
 		return m_observers[m][selector];
 	} else {
-		std::vector<Observer*> filtered;
+		std::vector<Observer<std::monostate>*> filtered;
 
 		for (auto& [k, v] : m_observers) {
 			filtered.insert(filtered.end(), v[selector].begin(), v[selector].end());
