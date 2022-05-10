46e8b980bb30564b0e8f9e968bd27772e217ed1e
camila314 <camden7306@gmail.com>
2022-05-10 09:18:14 -0500

    git status! temporary bandaid fix for macOS

--- src/core/Core.cpp
+++ src/core/Core.cpp
@@ -2,8 +2,37 @@
 #include <iostream>
 #include "Core.hpp"
 #include <Log.hpp>
+#include <platform.hpp>
 #include <map>
 
+#ifdef GEODE_IS_MACOS
+#warning Bandaid patch!
+#include <GDML.hpp>
+
+namespace geode::core::impl {
+	namespace {
+		auto& trampolines() {
+			static std::map<void*, HookContainer> ret;
+			return ret;
+		}
+	}
+
+	void* generateRawTrampoline(void* address) {
+        // std::cout << "address: " << address << std::endl;
+        // std::cout << "trampoline address is : " << trampolines()[address] << std::endl;
+        return (void*)trampolines()[address].getOriginal();
+	}
+
+	void addJump(void* at, void* to) {
+		trampolines()[at] = HookContainer((uintptr_t)at, (func_t)to);
+		trampolines()[at].enable();
+	}
+}
+
+bool geode::core::hook::initialize() {
+	return true;
+}
+#else
 namespace geode::core::impl {
 	namespace {
 		auto& trampolines() {
@@ -30,3 +59,4 @@ namespace geode::core::impl {
 bool geode::core::hook::initialize() {
 	return true;
 }
+#endif
