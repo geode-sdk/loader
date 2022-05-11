4383176b05b8ac452420ac9ce86357deb1ee444d
altalk23 <altalk23@users.noreply.github.com>
2022-05-12 01:19:21 +0300

    fix saving for macos

--- src/hooks/save.cpp
+++ src/hooks/save.cpp
@@ -2,8 +2,22 @@
 
 USE_GEODE_NAMESPACE();
 
+#ifdef GEODE_IS_MACOS
+class $modify(MenuLayer) {
+	void FLAlert_Clicked(FLAlertLayer* layer, bool btn1) {
+		if (btn1 == true && layer->getTag() == 0) {
+			Loader::getInternalMod()->log() << "Saving...";
+	        auto r = Loader::get()->saveSettings();
+	        if (!r) Loader::getInternalMod()->logInfo(r.error(), Severity::Error);
+	        Loader::getInternalMod()->log() << "Saved";
+		} 
+		MenuLayer::FLAlert_Clicked(layer, btn1);
+	} 
+};
+#else
+
 class $modify(AppDelegate) {
-    virtual void trySaveGame() {
+    void trySaveGame() {
         Loader::getInternalMod()->log() << "Saving...";
         auto r = Loader::get()->saveSettings();
         if (!r) Loader::getInternalMod()->logInfo(r.error(), Severity::Error);
@@ -11,3 +25,5 @@ class $modify(AppDelegate) {
         return AppDelegate::trySaveGame();
     }
 };
+
+#endif
