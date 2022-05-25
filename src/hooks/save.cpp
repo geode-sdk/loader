cf55a7b63a47f927bb665d208dcc383cf3997fce
altalk23 <altalk23@users.noreply.github.com>
2022-05-25 20:00:47 +0300

    use trysavegame for macos

--- src/hooks/save.cpp
+++ src/hooks/save.cpp
@@ -2,20 +2,6 @@
 
 USE_GEODE_NAMESPACE();
 
-#ifdef GEODE_IS_MACOS
-class $modify(MenuLayer) {
-	void FLAlert_Clicked(FLAlertLayer* layer, bool btn1) {
-		if (btn1 == true && layer->getTag() == 0) {
-			Loader::getInternalMod()->log() << "Saving...";
-	        auto r = Loader::get()->saveSettings();
-	        if (!r) Loader::getInternalMod()->logInfo(r.error(), Severity::Error);
-	        Loader::getInternalMod()->log() << "Saved";
-		} 
-		MenuLayer::FLAlert_Clicked(layer, btn1);
-	} 
-};
-#else
-
 class $modify(AppDelegate) {
     void trySaveGame() {
         Loader::getInternalMod()->log() << "Saving...";
@@ -25,5 +11,3 @@ class $modify(AppDelegate) {
         return AppDelegate::trySaveGame();
     }
 };
-
-#endif
